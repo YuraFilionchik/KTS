43cbc785f44e4052c38362dfd07829c3d3aaa762
FilionchykMaryia <59314336+FilionchykMaryia@users.noreply.github.com>
2020-03-25 18:33:32 +0300

    Dawngrade Framework from 4.6.1 to 4.5

--- KTS/UserForms/AddUserForm.cs
+++ KTS/UserForms/AddUserForm.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using ObservableCollectionExtensions = Microsoft.EntityFrameworkCore.ObservableCollectionExtensions;
+//using ObservableCollectionExtensions = Microsoft.EntityFrameworkCore.ObservableCollectionExtensions;
 
 namespace KTS
 {
